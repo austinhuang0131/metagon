a7fb0400fa29c0dc1fa6156a63827635fa3cf93b
Austin Huang <austinhuang0131@gmail.com>
2018-01-19 14:36:30 -0500

    oof

--- index.js
+++ index.js
@@ -1896,7 +1896,7 @@ bot.dialog('/', function (session) {
 // Setup Restify Server
 var server = express();
 server.post('/api/messages', connector.listen());
-server.post('/viber', viberChannel.listen());
+/*server.post('/viber', viberChannel.listen());*/
 server.post('/linebot', lineConnector.listen());
 server.listen(process.env.PORT || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
