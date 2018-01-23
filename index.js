1dfb3e00f32adb10d5e34c5ff703ded3cd8b23d2
Austin Huang <austinhuang0131@gmail.com>
2018-01-23 16:16:14 -0500

    tabarnaq

--- index.js
+++ index.js
@@ -2116,3 +2116,11 @@ server.post('/vk', vk.listen());
 server.listen(process.env.PORT || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
+
+// Errors
+process.on('unhandledRejection', (reason, p) => {
+  	console.log('Unhandled Rejection at:', p, 'reason:', reason);
+});
+process.on('uncaughtException', (reason) => {
+  	console.log('Uncaught Exception:', reason);
+});
