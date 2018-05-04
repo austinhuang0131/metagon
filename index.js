0c015f84d029aedae986d9b0e9644a4aab467973
Austin Huang <austinhuang0131@gmail.com>
2018-05-04 08:11:55 -0400

    hrm

--- index.js
+++ index.js
@@ -2039,8 +2039,8 @@ bot.dialog('/', function (session) {
 var server = express();
 server.use(bodyParser.json({type: "*/*"}));
 server.post('/api/messages', connector.listen());
-server.post('/linebot', lineConnector.listen);
-// server.post('/cisco', cisco.listen);
+server.use('/linebot', lineConnector.listen);
+server.post('/cisco', cisco.listen);
 server.listen(process.env.PORT || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
