9e340b3bf8767d9ac22405f482a5abca49b3bc09
Austin Huang <austinhuang0131@gmail.com>
2018-05-21 15:29:18 -0400

    oof

--- index.js
+++ index.js
@@ -2038,8 +2038,8 @@ bot.dialog('/', function (session) {
 var server = express();
 server.use(bodyParser.json({type: "*/*"}));
 server.post('/api/messages', connector.listen());
-server.post('/line', lineConnector.listen());
-server.post('/cisco', cisco.listen());
+server.post('/line', lineConnector.listen);
+server.post('/cisco', cisco.listen);
 server.listen(process.env.PORT || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
 });
