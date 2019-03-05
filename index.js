2d484fb2cfee1491795b90a3b3b3137dbb7e96da
Austin Huang <austinhuang0131@gmail.com>
2019-03-05 13:34:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2010,7 +2010,6 @@ bot.dialog('/test2', function (session) {
 var server = express();
 server.use(bodyParser.json({type: "*/*"}));
 server.post('/api/messages', connector.listen());
-server.post('/line', lineConnector.listen);
 //server.post('/cisco', cisco.listen);
 server.listen(process.env.PORT || 5000, function () {
     console.log('%s listening to %s', server.name, server.url); 
